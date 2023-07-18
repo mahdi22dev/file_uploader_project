2b4be2d8e63f47b4a4974b929722cf0147179ca0
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-07-18 02:32:17 +0100

    files

--- src/component/uploadloading/UploadLoading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "./uploadloading.module.css";
-
-const UploadLoading = () => {
-  return <div className={styled.loading}>...uploading</div>;
-};
-
-export default UploadLoading;
