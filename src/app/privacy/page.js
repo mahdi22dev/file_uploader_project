04feee736525308a6d44c75cc770cc79758cafde
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-08-06 17:03:52 +0100

    privacy page

---
+++ src/app/privacy/page.js
@@ -0,0 +1,11 @@
+import styled from "./page.module.css";
+
+export default async function Page() {
+  return (
+    <>
+      <main className={styled.container}>
+        <h1>privacy page</h1>
+      </main>
+    </>
+  );
+}
