deff189972c6b2878bf8045381cc4f873abf6417
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-05 21:05:16 +0100

    links design

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -1,11 +1,14 @@
 import { copyToclipBoard, notifyClipBoard } from "@/lib/utils/utils";
+import Link from "next/link";
 import React from "react";
 import { FaCopy } from "react-icons/fa";
 
 const Links = ({ link, className }) => {
   return (
     <div style={{ minWidth: "fit-content" }} className={className}>
-      <p style={{ padding: "10px", paddingLeft: "5px" }}>{link.link}</p>
+      <p style={{ padding: "10px", paddingLeft: "5px" }}>
+        <Link href={link.link}>{link.name}</Link>
+      </p>
       <FaCopy
         onClick={() => {
           copyToclipBoard(link.link);
