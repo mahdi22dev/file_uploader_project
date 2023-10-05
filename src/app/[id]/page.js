e25c38a0f0a91c5c3ada451e0504a10bea47cb63
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-10-05 02:41:07 +0100

    retrive meta data

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -2,25 +2,21 @@ import React from "react";
 import styled from "./page.module.css";
 import { SlCloudDownload } from "react-icons/sl";
 import { storage } from "@/lib/firebase";
-import { getDownloadURL, list, listAll, ref } from "firebase/storage";
+import { getDownloadURL, getMetadata, ref } from "firebase/storage";
 import NotFound from "./not-found";
-
-export async function generateStaticParams({ params }) {
-  const response = await fetch(
-    "https://firebasestorage.googleapis.com/v0/b/file-uploader-a0f03.appspot.com/o",
-    { cache: "no-cache" }
-  );
-  const data = await response.json();
-  return data.items.map((file) => ({
-    id: file.name,
-  }));
-}
 
 export default async function Page({ params }) {
   const filename = params.id;
   const storageRef = ref(storage, filename);
   const downloadURL = await getDownloadURL(storageRef);
+  // const fileref = ref(storage, "/");
+  const metadata = await getMetadata(storageRef);
+  console.log(metadata);
 
+  const fileInfo = {
+    name: metadata.customMetadata.name,
+    size: "will be added with meta data",
+  };
   if (!downloadURL) {
     return NotFound();
   }
@@ -33,8 +29,8 @@ export default async function Page({ params }) {
         href={downloadURL}
         download={"songmp3.mp3_u_srgBNYxwHhFZ3be5LY1"}
       >
-        <h2>File Name: {params.id}</h2>
-        {/* <h2>file size: {size?.readable}</h2> */}
+        <h2>File Name: {fileInfo.name}</h2>
+        <h2>file size: {fileInfo?.size}</h2>
         <a
           className={styled.button}
           href={downloadURL}
